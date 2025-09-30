0daa46044e89e581e4b5d27d7f6b31368862d310
Luzatti-alt <lucasguzatti4@gmail.com>
2025-09-30 11:25:54 -0300

    inicio da impementação de funcionalidade

---
+++ js/loja.js
@@ -0,0 +1,6 @@
+const adquirirDemo = document.getElementById('adquirir_demo');
+const comprarJogoBase = document.getElementById('comprar_jogo_base');
+const comprarPack = document.getElementById('comprar_pack');
+const comprarEdicao = document.getElementById('comprar_edicao');
+const comprarDlc = document.getElementById('comprar_dlc');
+const denunciar = document.getElementById('denunciar');
