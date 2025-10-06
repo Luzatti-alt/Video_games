749af48dd7b8048568bdcf619482c62361310462
luzatti-alt <lucasguzatti4@gmail.com>
2025-10-06 10:30:15 -0300

    "funcionalidade"

---
+++ js/conta_pagina.js
@@ -0,0 +1,9 @@
+const editar_perfil = document.getElementById('editar_perfil');
+const enviar = document.getElementById('enviar');
+enviar.addEventListener('click', () => {
+    alert('enviando comentario');
+});
+editar_perfil.addEventListener('click', () => {
+    alert('editou perfil');
+});
+
