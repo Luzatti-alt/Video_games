52766a0c77824e95c2f3d32aa07b1b6967d704b9
Joao-Gabriel-Kosctiuk <jgkosctiuk@gmail.com>
2025-11-02 21:45:07 -0300

    Correções e adições 1.4 (agora sim 1.4)

--- js/loja.js
+++ js/loja.js
@@ -1,10 +1,10 @@
-const adquirirDemo = document.getElementById('adquirir_demo');
-const comprarJogoBase = document.getElementById('comprar_jogo_base');
-const comprarPack = document.getElementById('comprar_pack');
-const comprarEdicao = document.getElementById('comprar_edicao');
-const comprarDlc = document.getElementById('comprar_dlc');
-const denunciar = document.getElementById('denunciar_but');
-const pesquisa = document.getElementById('pesquisar');
+// const adquirirDemo = document.getElementById('adquirir_demo');
+// const comprarJogoBase = document.getElementById('comprar_jogo_base');
+// const comprarPack = document.getElementById('comprar_pack');
+// const comprarEdicao = document.getElementById('comprar_edicao');
+// const comprarDlc = document.getElementById('comprar_dlc');
+// const denunciar = document.getElementById('denunciar_but');
+// const pesquisa = document.getElementById('pesquisar');
 //carrosel
 let indice = 0;
 let slide = document.getElementById("slide");
@@ -17,30 +17,30 @@ setInterval(() => {
     indice++; // Incrementa o índice para a próxima imagem
 }, 3000);
 
-//eventos dos botões
-pesquisa.addEventListener('click', () => {
-    alert('Pesquisar');
-});
-adquirirDemo.addEventListener('click', () => {
-    alert('Comprou Demo');
-});
-comprarJogoBase.addEventListener('click', () => {
-    alert('comprou Jogo Base');
-});
+// //eventos dos botões
+// pesquisa.addEventListener('click', () => {
+//     alert('Pesquisar');
+// });
+// adquirirDemo.addEventListener('click', () => {
+//     alert('Comprou Demo');
+// });
+// comprarJogoBase.addEventListener('click', () => {
+//     alert('comprou Jogo Base');
+// });
 
-comprarPack.addEventListener('click', () => {
-    alert('comprou Pack');
-});
+// comprarPack.addEventListener('click', () => {
+//     alert('comprou Pack');
+// });
 
-comprarEdicao.addEventListener('click', () => {
-    alert('comprou Edição');
-});
+// comprarEdicao.addEventListener('click', () => {
+//     alert('comprou Edição');
+// });
 
-comprarDlc.addEventListener('click', () => {
-    alert('comprou DLC');
-});
+// comprarDlc.addEventListener('click', () => {
+//     alert('comprou DLC');
+// });
 
-denunciar.addEventListener('click', () => {
-    alert('Denunciar');
-});
+// denunciar.addEventListener('click', () => {
+//     alert('Denunciar');
+// });
 
