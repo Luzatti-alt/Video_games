07f6df6e432ef681f04b7b82145ea7404d52dd99
luzatti-alt <lucasguzatti4@gmail.com>
2025-11-05 08:42:36 -0300

    sistema inicial de criação de contas  via api

--- api.py
+++ api.py
@@ -9,9 +9,24 @@ engine = create_engine('sqlite:///meubanco.db', echo=True)
 Session = sessionmaker(bind=engine)
 session = Session()
 
-@api.route("/", methods=["GET"])
+@db.route("/", methods=["GET"])
 def root():
     return "só o root da api"
+@db.route("/criar_conta", methods=["POST"])
+def criar_conta():
+    #js mandar como json _> pegar os dados do json
+    dados = request.get_json()
+    email = dados.get("email")
+    nick = dados.get("nick")
+    #criar verificação de nick
+    senha = dados.get("senha")
+    conta = session.query(conta).filter_by(email=email).first()
+    conta_senha = bcrypt.generate_password_hash(dados["senha"]).decode("utf-8")
+    nova_conta = conta(nick=nick,email=email,senha=conta_senha)
+    session.add(nova_conta)
+    session.commit()
+    return jsonify({"status": "ok", "mensagem": "Conta criada com sucesso!"})
+
 #iniciar flask
 if __name__ == "__main__":
     db.run(host='0.0.0.0')
