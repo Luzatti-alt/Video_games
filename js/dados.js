23d7f35e35b677ee336dda35f672efc08d455f44
luzatti-alt <lucasguzatti4@gmail.com>
2025-11-05 10:23:55 -0300

    arrumando uma parte

--- js/dados.js
+++ js/dados.js
@@ -1,5 +1,7 @@
 //aqui ele fara alterações conforme a conta
 function carregarInfo() {
-    document.getElementById("nick").textContent = dados.nome;
+    const Nick = localStorage.getItem("nick");//so para diferenciar o item salvo
+    //com o item aqui
+    document.getElementById("nick").textContent = dados.Nick;
 }
-document.getElementById("nick").textContent = dados.nome;
+document.getElementById("nick").textContent = "bem vindo,"+dados.Nick;
