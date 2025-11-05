2bd6dcdba476fdb5d285aba1aedcff6ff0d2d7a9
luzatti-alt <lucasguzatti4@gmail.com>
2025-11-05 14:33:09 -0300

    so o suficiente para eu diferenciar e poder adicionar a funcionalidade

--- js/loja_deltarune.js
+++ js/loja_deltarune.js
@@ -1,7 +1,7 @@
 //carrosel
 let indice = 0;
 let slide = document.getElementById("slide");
-let imagens = ["imagens/deltarune(2).avif","imagens/silksong-gameplay(1).jpg","imagens/silksong-gameplay(2).jpg","imagens/silksong-gameplay(3).webp","imagens/silksong-gameplay(4).jpg"];
+let imagens = ["imagens/deltarune(2).avif","imagens/deltarune.jpg","imagens/deltarune(2).jpg","imagens/silksong-gameplay(3).webp","imagens/silksong-gameplay(4).jpg"];
 setInterval(() => {
     if (indice >= imagens.length) {
         indice = 0; // Reinicia o índice para 0 quando chega ao final do array
