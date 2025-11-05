5d424232a7fe93a5f81208660dadd4b3cba00478
luzatti-alt <lucasguzatti4@gmail.com>
2025-11-05 08:36:41 -0300

    api base minima

--- api.py
+++ api.py
@@ -3,3 +3,15 @@ from flask_bcrypt import Bcrypt #encriptar a senha
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from contas import Contas,Biblioteca
+db = Flask(__name__)
+bcrypt = Bcrypt(db)
+engine = create_engine('sqlite:///meubanco.db', echo=True)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+@api.route("/", methods=["GET"])
+def root():
+    return "só o root da api"
+#iniciar flask
+if __name__ == "__main__":
+    db.run(host='0.0.0.0')
