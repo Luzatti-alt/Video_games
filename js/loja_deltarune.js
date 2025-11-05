9110421342c975b086c7ca5d9baa2c2b1e84f8ca
luzatti-alt <lucasguzatti4@gmail.com>
2025-11-05 14:28:34 -0300

    começando a fazer outra pagina de jogo

---
+++ js/loja_deltarune.js
@@ -0,0 +1,11 @@
+//carrosel
+let indice = 0;
+let slide = document.getElementById("slide");
+let imagens = ["imagens/deltarune(2).avif","imagens/silksong-gameplay(1).jpg","imagens/silksong-gameplay(2).jpg","imagens/silksong-gameplay(3).webp","imagens/silksong-gameplay(4).jpg"];
+setInterval(() => {
+    if (indice >= imagens.length) {
+        indice = 0; // Reinicia o índice para 0 quando chega ao final do array
+    }
+    slide.src = imagens[indice];
+    indice++; // Incrementa o índice para a próxima imagem
+}, 3000);
